1cdc7a85f9abc0d317256030b9c42d2dd2e18d1a
cvssn <90540827+cvssn@users.noreply.github.com>
2026-07-30 14:05:27 -0300

    adição de middleware de logging para headers de http

---
+++ pkg/logging/middleware.go
@@ -0,0 +1,26 @@
+package logging
+
+import (
+	"net/http"
+
+	"github.com/keepchasingheaven/encore-workspaces-proxy/internal/logz"
+	"go.uber.org/zap"
+)
+
+func NewMiddleware(logger *zap.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			recorder := newResponseRecorder(w)
+			next.ServeHTTP(recorder, r)
+
+			logger.Info("processed HTTP request",
+				logz.HTTPPath(r.URL.Path),
+				logz.HTTPIp(r.RemoteAddr),
+				logz.HTTPStatus(recorder.status),
+				logz.HTTPHost(r.Host),
+				logz.HTTPMethod(r.Method),
+				logz.HTTPScheme(r.URL.Scheme)
+			)
+		})
+	}
+}
